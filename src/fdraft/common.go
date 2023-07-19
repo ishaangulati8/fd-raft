e6f68831425253cc7d66e840b062897b5165c54d
Ishaan Gulati <ishaangulati@Ishaans-MacBook-Pro.local>
2023-07-19 11:16:55 -0700

    Remove time.sleep() after client warm up

--- src/fdraft/common.go
+++ src/fdraft/common.go
@@ -1,41 +1,3 @@
-// package kvraft
-
-// const (
-// 	OK             = "OK"
-// 	ErrNoKey       = "ErrNoKey"
-// 	ErrWrongLeader = "ErrWrongLeader"
-// 	ErrNoLeader    = "ErrNoLeader"
-// )
-
-// type Err string
-
-// // Put or Append
-// type PutAppendArgs struct {
-// 	Key   string
-// 	Value string
-// 	Op    string // "Put" or "Append"
-// 	// You'll have to add definitions here.
-// 	// Field names must start with capital letters,
-// 	// otherwise RPC will break.
-// 	ClientId, Seq int64
-// }
-
-// type PutAppendReply struct {
-// 	Err Err
-// }
-
-// type GetArgs struct {
-// 	Key string
-// 	// You'll have to add definitions here.
-// 	ClientId, Seq int64
-// }
-
-// type GetReply struct {
-// 	Err   Err
-// 	Value string
-// }
-
-
 package fdraft
 
 type Err string
@@ -60,10 +22,7 @@ const (
 type PutAppendArgs struct {
 	Key   string
 	Value string
-	Op    opType // "Put" or "Append"
-	// You'll have to add definitions here.
-	// Field names must start with capital letters,
-	// otherwise RPC will break.
+	Op    opType
 	ClientId int64 // id of client
 	OpId     int   // client operation id
 }
@@ -74,7 +33,6 @@ type PutAppendReply struct {
 
 type GetArgs struct {
 	Key string
-	// You'll have to add definitions here.
 	ClientId int64 // id of client
 	OpId     int   // client operation id
 }
